9bbab153daef29e619d69396debf5bac420b5678
Alouiscious Media <info@reneesoul.com>
2019-10-03 05:41:34 -0700

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,54 +1,86 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require 'pry'
 
 class InteractiveRecord
   def self.table_name
     ActiveSupport::Inflector.tableize(self.to_s)
   end
 
-  def self.column_name
-    binding.pry
+  def self.column_names
     sql = "PRAGMA table_info('#{table_name}')"
-    column_names = DB[:conn].execute(sql).map {|column| column["name"]}
-      column_names.each {|column| attr_accessor column.to_sym}
-      column_names.compact
-    end
-  }
+    pragma_info = DB[:conn].execute(sql).map {|column| column["name"]}
+    # attrib_names = []
+    # attrib_names << 
+    pragma_info.each {|attrib| attr_accessor attrib.to_sym}
+    pragma_info.compact
+    
   end
+
+  # def self.column_names
+  #   sql = "PRAGMA table_info('#{table_name}')"
+  #   pragma_info = DB[:conn].execute(sql)
+  #   attrib_names = []
+  #   pragma_info.each do |attrib| 
+  #     attrib_names << attrib["name"].to_sym
+  #   end
+  #   attr_accessor attrib_names.compact
+    
+  # end
 
   def self.find_by_name(name)
     sql = <<-SQL
       SELECT * FROM #{table_name} WHERE name = ?
       SQL
-      DB[:conn].execute(sql)
+      DB[:conn].execute(sql, name)
   end
 
-  def self.find_by _attributes(attributes)
+  def self.find_by(attributes)
+   conditions = attributes.keys.map {|k| "#{k.to_s} = ?"}.join(",")
     sql = <<-SQL
-      SELECT * FROM #{table_name} WHERE #{attributes.keys}
+      SELECT * FROM #{table_name} WHERE #{conditions}
     SQL
+ 
+        DB[:conn].execute(sql, attributes.values)  
+  end
 
   def initialize(attributes = {})
     attributes.each do |attribute, value|
+      # binding.pry
       self.send("#{attribute}=", value)
     end
+    self
+  end
 
-    def table_name_for_insert
-      self.class.table_name
-    end
-    
-    def column_names_for_insert
-      self.class.column_names.delete_if{|column| column == 'id'}.join(", ")
-    end
+  def table_name_for_insert
+    self.class.table_name
+  end
+  
+  def col_names_for_insert
+    self.class.column_names.delete_if {|column| column == 'id'}.join(", ")
+      # self.class.column_names_for_insert
+  end
 
-    def values_for_insert
-      
+  def values_for_insert
+    attributes = {}
+    self.col_names_for_insert.split(', ').each do |attr|
+      attributes[attr] = self.send(attr) if self.send(attr) #don't add key value pairs where attribute value is nil
     end
+    attributes.values.map{|v| "'#{v}'"}.join(', ')
+  end
 
-    def save
-      sql = <<-SQL
-        INSERT INTO students (name, grade) VALUES ()
-    end
+  def save
+    # binding.pry
+    sql = <<-SQL
+      INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})
+    SQL
+
+    DB[:conn].execute(sql)
+    self.id = DB[:conn].execute("select last_insert_rowid() FROM students").first.values.first
+    # self
+    # self.class.table_name
+
+  end
 
 
 
@@ -103,20 +135,20 @@ end
 #   end
 # ​
 #   def values_for_insert 
-#     attributes = {}
-#     self.col_names_for_insert.split(', ').each do |attr|
-#       attributes[attr] = self.send(attr) if self.send(attr) #don't add key value pairs where attribute value is nil
-#     end
-#     attributes.values.map{|v| "'#{v}'"}.join(', ')
+    # attributes = {}
+    # self.col_names_for_insert.split(', ').each do |attr|
+    #   attributes[attr] = self.send(attr) if self.send(attr) #don't add key value pairs where attribute value is nil
+    # end
+    # attributes.values.map{|v| "'#{v}'"}.join(', ')
 #   end
 # ​
 #   def save 
-#     sql = <<-SQL
-#       INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})
-#     SQL
-#     DB[:conn].execute(sql)
-#     self.id = DB[:conn].execute("select last_insert_rowid() FROM students").first.values.first
-#     self
+    # sql = <<-SQL
+    #   INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})
+    # SQL
+    # DB[:conn].execute(sql)
+    # self.id = DB[:conn].execute("select last_insert_rowid() FROM students").first.values.first
+    # self
 #   end
 # end
 # Collapse
